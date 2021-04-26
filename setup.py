d2eef1456cdaaa461efb2e727a41d73fdef512fb
Benjamin Galliot <b.g01lyon@gmail.com>
2021-04-26 13:30:59 +0200

    Mise à jour de version et nettoyage du fichier d’installation.

--- setup.py
+++ setup.py
@@ -3,24 +3,17 @@
 
 from setuptools import setup, find_packages
 
-__author__ = 'Benjamin Galliot'
-__copyright__ = "Copyleft"
-__license__ = "Spéciale"
-__version__ = "0.4"
-__email__ = "b.g01lyon@gmail.com"
-__status__ = "Development Status :: 3 - Alpha"
-
 setup(name='lexika',
-      version=__version__,
-      description='Logiciel de création de dictionnaires multilingues avec interface graphique.',
+      version="1.0",
+      description='Logiciel de création de dictionnaires multilingues.',
       author='Benjamin Galliot',
       author_email='b.g01lyon@gmail.com',
-      url='https://bitbucket.org/BenjaminGalliot/lexika',
+      url='https://gitlab.com/BenjaminGalliot/Lexika/',
       packages=find_packages(),
-      install_requires=['cchardet', 'lxml', 'regex'],
+      install_requires=['cchardet', 'colorama', 'lxml', 'regex', 'pyyaml'],
       include_package_data=True,
       entry_points={'console_scripts': ['lexika=lexika.base:démarrer_lexika'],},
-      classifiers=["Development Status :: 2 - Pre-Alpha",
+      classifiers=["Development Status :: 5 - Production/Stable",
                    "Intended Audience :: Science/Research",
                    "Intended Audience :: End Users/Desktop",
                    "Intended Audience :: Other Audience",
@@ -36,6 +29,4 @@ setup(name='lexika',
                    "Topic :: Text Processing :: Markup :: XML",
                    "Topic :: Utilities"],
       keywords="linguistique dictionnairique lexicographie linguistics dictionaric lexicography",
-
-
 )
